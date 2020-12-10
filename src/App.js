0a668236d4a9329d94796c832e29d21ff34e27b2
Ivan Lencina <ivanlencina_14@outlook.com>
2020-12-10 11:39:15 -0300

    desafío navbar (mod)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar'; //el componente que trato de incorporar no aparece en pantalla, excepto que borre "App". cómo lo resuelvo?
-
-NavBar();
+import NavBar from './components/NavBar/NavBar.js';
+import './components/NavBar/NavBar.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ¡Hola mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavBar/>
   );
 }
 
